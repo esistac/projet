d380e3429f910cd2cd38d44adbababa9a0dea362
esistac <emilien.sistac@estiam.com>
2026-06-27 19:45:27 +0200

    chore: trigger fresh sonarqube analysis

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -7,3 +7,8 @@ output "container_name" {
   description = "Le nom du conteneur de l'application"
   value       = docker_container.app_container.name
 }
+
+output "staging_url" {
+  description = "L'URL locale pour tester l'API FastAPI"
+  value       = "http://localhost:8000"
+}

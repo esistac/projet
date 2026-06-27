f0886b779417d0c844d1b2cd99291b9f02a5bcc3
esistac <emilien.sistac@estiam.com>
2026-06-27 19:30:50 +0200

    chore: trigger fresh sonarqube analysis

--- src/main.py
+++ src/main.py
@@ -85,4 +85,6 @@ def get_tasks():
     latency = time.time() - start_time
     REQUEST_LATENCY.labels(method="GET", endpoint="/tasks").observe(latency)
     return tasks_db
+
+
 # Force SonarQube update
